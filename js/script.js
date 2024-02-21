575a6b2e8fad4597f5c16b1be9e38523a6952f30
lmatani <lmatarrubia@gmail.com>
2024-02-21 19:22:49 +0100

    Entregable_2_add_elements_dom_sprint_3

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
  // Aquí tu código
+
+ const btnAgregar = document.getElementById('agregar');
+ let elementoLista = document.createElement('li');
+ let lista = document.getElementById('lista');
+
+ btnAgregar.addEventListener('click', function(){
+    let elementoLista= document.createElement('li');
+    elementoLista.textContent = prompt('Introduce un elemento a la lista:');
+    lista.appendChild(elementoLista);
+ });
+
+ const btnBorrar = document.getElementById('borrar_lista');
+ btnBorrar.addEventListener('click', function(){
+    while (lista.firstChild) {
+        lista.removeChild(lista.firstChild);
+    }
+ });
+
+ const btnBorrarElem = document.getElementById('borrar_elemento');
+ btnBorrarElem.addEventListener('click', function(){
+    lista.removeChild(lista.lastChild);
+ });
+
+
+
